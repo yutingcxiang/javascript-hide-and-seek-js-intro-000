f1357629d575176b465eb8a527c17381cf4a7d0e
Christine Xiang <yutingcxiang@gmail.com>
2018-06-20 00:26:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var item = document.querySelectorAll(".ranked-list");
+  for (var i=0; i<item.length; i++) {
+    item[i].innerHTML = parseInt(item[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var start = document.querySelectorAll("div#grand-node");
+  for (i=0; i<start.length;i++) {
+    var node = document.querySelector(node[0]);
+  }
+}
